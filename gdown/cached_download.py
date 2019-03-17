35741525d0b4b4b806c13750cd4187b69c34ba5a
Kentaro Wada <www.kentaro.wada@gmail.com>
2019-03-17 13:34:40 +0000

    Improve message of cached_download

--- gdown/cached_download.py
+++ gdown/cached_download.py
@@ -79,11 +79,18 @@ def cached_download(url, path=None, md5=None, quiet=False, postprocess=None):
     temp_root = tempfile.mkdtemp(dir=cache_root)
     try:
         temp_path = osp.join(temp_root, 'dl')
+
+        if not quiet:
+            msg = 'Cached Downloading'
+            if path:
+                msg = '{}: {}'.format(msg, path)
+            else:
+                msg = '{}...'.format(msg)
+            print(msg, file=sys.stderr)
+
         download(url, temp_path, quiet=quiet)
         with filelock.FileLock(lock_path):
             shutil.move(temp_path, path)
-        if not quiet:
-            print('Saved to: {}'.format(path), file=sys.stderr)
     except Exception:
         shutil.rmtree(temp_root)
         raise
