4067e08640300b2e32c678471a38b64117b0781e
Shun Hasegawa <pazeshun3684@gmail.com>
2020-01-10 18:43:18 +0900

    Add test of download for usage before https://github.com/wkentaro/gdown/pull/32

---
+++ tests/test_download.py
@@ -0,0 +1,11 @@
+from gdown.download import download
+import os
+
+
+def test_download():
+    url = 'https://raw.githubusercontent.com/wkentaro/gdown/3.1.0/gdown/__init__.py'  # NOQA
+    output = '/tmp/gdown_r'
+
+    # Usage before https://github.com/wkentaro/gdown/pull/32
+    assert download(url, output, quiet=False) == output
+    os.remove(output)
