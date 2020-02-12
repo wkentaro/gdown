a09276943d45b3d825f8a678f399b0729f9f8d2f
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-02-12 02:14:06 +0000

    Apply black for tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -4,8 +4,8 @@ from gdown.download import download
 
 
 def test_download():
-    url = 'https://raw.githubusercontent.com/wkentaro/gdown/3.1.0/gdown/__init__.py'  # NOQA
-    output = '/tmp/gdown_r'
+    url = "https://raw.githubusercontent.com/wkentaro/gdown/3.1.0/gdown/__init__.py"  # NOQA
+    output = "/tmp/gdown_r"
 
     # Usage before https://github.com/wkentaro/gdown/pull/32
     assert download(url, output, quiet=False) == output
--- tests/test_parse_url.py
+++ tests/test_parse_url.py
@@ -3,29 +3,29 @@ import pytest
 
 
 def test_parse_url():
-    file_id = '0B_NiLAzvehC9R2stRmQyM3ZiVjQ'
+    file_id = "0B_NiLAzvehC9R2stRmQyM3ZiVjQ"
 
     # list of (url, expected, check_warn)
     urls = [
         (
-            'https://drive.google.com/open?id={}'.format(file_id),
+            "https://drive.google.com/open?id={}".format(file_id),
             (file_id, False),
             True,
         ),
         (
-            'https://drive.google.com/uc?id={}'.format(file_id),
+            "https://drive.google.com/uc?id={}".format(file_id),
             (file_id, True),
             False,
         ),
         (
-            'https://drive.google.com/file/d/{}/view?usp=sharing'.format(
+            "https://drive.google.com/file/d/{}/view?usp=sharing".format(
                 file_id
             ),  # NOQA
             (file_id, False),
             True,
         ),
         (
-            'https://drive.google.com/a/jsk.imi.i.u-tokyo.ac.jp/uc?id={}&export=download'.format(  # NOQA
+            "https://drive.google.com/a/jsk.imi.i.u-tokyo.ac.jp/uc?id={}&export=download".format(  # NOQA
                 file_id
             ),
             (file_id, True),
