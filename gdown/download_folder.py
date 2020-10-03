b11418d6b957a3c202426a956628db107ce1afc2
motivationalreposter <72241740+motivationalreposter@users.noreply.github.com>
2020-10-03 21:53:53 +0800

    Add directory structure

--- gdown/download_folder.py
+++ gdown/download_folder.py
@@ -8,16 +8,15 @@ if sys.version_info.major < 3:
     from pathlib2 import Path
 else:
     from pathlib import Path
-
-folder_root = Path()
-
 client = requests.session()
 
+folders_url = "https://drive.google.com/drive/folders/"
+files_url = "https://drive.google.com/uc?id="
+folder_type = "application/vnd.google-apps.folder"
 
-def download_folder(
-    folder, quiet=False, proxy=None, speed=None, use_cookies=True
-):
-    """Download entire folder from URL.
+
+def get_folder_list(folder, quiet=False, use_cookies=True):
+    """Get folder structure of Google Drive folder URL.
 
     Parameters
     ----------
@@ -26,11 +25,7 @@ def download_folder(
         Must be of the format 'https://drive.google.com/drive/folders/{url}'.
     quiet: bool, optional
         Suppress terminal output.
-    proxy: str, optional
-        Proxy.
-    speed: float, optional
-        Download byte size per second (e.g., 256KB/s = 256 * 1024).
-    use_cookies: bool
+    use_cookies: bool, optional
         Flag to use cookies. Default is True.
 
     Returns
@@ -39,22 +34,11 @@ def download_folder(
         Returns False if the download completed unsuccessfully.
         May be due to invalid URLs, permission errors, rate limits, etc.
     folder_list: dict
-        Returns the directory structure of the folder.
-
-    Example
-    -------
-    gdown.download_folder(
-        "https://drive.google.com/drive/folders/" +
-        "1ZXEhzbLRLU1giKKRJkjm8N04cO_JoYE2",
-        use_cookies=True
-    )
+        Returns the folder structure of the Google Drive folder.
     """
     return_code = True
 
     folder_list = {}
-
-    folders_url = "https://drive.google.com/drive/folders/"
-    files_url = "https://drive.google.com/uc?id="
 
     folder_page = client.get(folder)
 
@@ -77,9 +61,9 @@ def download_folder(
         .replace("null", '"null"')
     )
 
-    folder_list["file_name"] = folder_soup.title.contents
+    folder_list["file_name"] = folder_soup.title.contents[0][:-15]
     folder_list["file_id"] = folder[39:]
-    folder_list["file_type"] = "application/vnd.google-apps.folder"
+    folder_list["file_type"] = folder_type
     folder_list["file_contents"] = []
 
     folder_file_list = [i[0] for i in folder_arr[0]]
@@ -87,7 +71,13 @@ def download_folder(
     folder_type_list = [i[3] for i in folder_arr[0]]
 
     for file in range(len(folder_file_list)):
-        if folder_type_list[file] != "application/vnd.google-apps.folder":
+        if folder_type_list[file] != folder_type:
+            if not quiet:
+                print(
+                    "Processing file",
+                    folder_file_list[file],
+                    folder_name_list[file],
+                )
             folder_list["file_contents"].append(
                 {
                     "file_name": folder_name_list[file],
@@ -96,35 +86,130 @@ def download_folder(
                     "file_contents": None,
                 }
             )
-            return_code = download(
-                files_url + folder_file_list[file],
-                output=folder_name_list[file],
-                quiet=True,
-                proxy=proxy,
-                speed=speed,
-                use_cookies=use_cookies,
-            )
-            if not quiet:
-                print(
-                    files_url + folder_file_list[file], folder_name_list[file]
-                )
             if not return_code:
                 return return_code, None
         else:
             if not quiet:
                 print(
-                    "Processing folder",
+                    "Retrieving folder",
+                    folder_file_list[file],
                     folder_name_list[file],
-                    "(" + folders_url + folder_file_list[file] + ")",
                 )
-            return_code, directory_structure = download_folder(
+            return_code, folder_structure = get_folder_list(
                 folders_url + folder_file_list[file],
-                quiet=quiet,
-                proxy=proxy,
-                speed=speed,
                 use_cookies=use_cookies,
             )
             if not return_code:
                 return return_code, None
-            folder_list["file_contents"].append(directory_structure)
+            folder_list["file_contents"].append(folder_structure)
     return return_code, folder_list
+
+
+def get_directory_structure(directory, previous_path):
+    """Converts a Google Drive folder structure into a local directory list.
+
+    Parameters
+    ----------
+    directory: dict
+        Dictionary containing the Google Drive folder structure.
+    previous_path: pathlib.Path
+        Path containing the parent's file path.
+
+    Returns
+    -------
+    directory_structure: list
+        List containing a tuple of the files' ID and file path.
+    """
+    directory_structure = []
+    for file in directory["file_contents"]:
+        if file["file_type"] == folder_type:
+            for i in get_directory_structure(
+                file, previous_path / file["file_name"]
+            ):
+                directory_structure.append(i)
+        elif file["file_contents"] is None:
+            directory_structure.append(
+                (file["file_id"], previous_path / file["file_name"])
+            )
+    return directory_structure
+
+
+def download_folder(
+    folder, output=None, quiet=False, proxy=None, speed=None, use_cookies=True
+):
+    """Downloads entire folder from URL.
+
+    Parameters
+    ----------
+    url: str
+        URL of the Google Drive folder.
+        Must be of the format 'https://drive.google.com/drive/folders/{url}'.
+    output: str, optional
+        String containing the path of the output folder.
+        Defaults to current working directory.
+    quiet: bool, optional
+        Suppress terminal output.
+    proxy: str, optional
+        Proxy.
+    speed: float, optional
+        Download byte size per second (e.g., 256KB/s = 256 * 1024).
+    use_cookies: bool, optional
+        Flag to use cookies. Default is True.
+
+    Returns
+    -------
+    return_code: bool
+        Returns False if the download completed unsuccessfully.
+        May be due to invalid URLs, permission errors, rate limits, etc.
+
+    Example
+    -------
+    gdown.download_folder(
+        "https://drive.google.com/drive/folders/" +
+        "1ZXEhzbLRLU1giKKRJkjm8N04cO_JoYE2",
+        use_cookies=True
+    )
+    """
+    if not quiet:
+        print("Retrieving folder list")
+    return_code, folder_list = get_folder_list(
+        folder,
+        quiet=quiet,
+        use_cookies=False,
+    )
+
+    if not return_code:
+        return return_code
+    if not quiet:
+        print("Retrieving folder list completed")
+        print("Building directory structure")
+    if output is None:
+        output = Path.cwd()
+    else:
+        output = Path(output)
+    directory_structure = get_directory_structure(
+        folder_list,
+        output / folder_list["file_name"],
+    )
+
+    if not quiet:
+        print("Building directory structure completed")
+    for file in directory_structure:
+        file[1].parent.mkdir(parents=True, exist_ok=True)
+
+        return_code = download(
+            file[0],
+            output=str(file[1]),
+            quiet=quiet,
+            proxy=proxy,
+            speed=speed,
+            use_cookies=use_cookies,
+        )
+
+        if not return_code:
+            if not quiet:
+                print("Download ended unsuccessfully")
+            return return_code
+    if return_code and not quiet:
+        print("Download completed")
+    return return_code
