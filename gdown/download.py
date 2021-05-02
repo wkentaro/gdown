504d38ac7db5b6a2acaf29828ed0588a65c6fdfb
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-05-02 18:41:35 +0900

    Fix format

--- gdown/download.py
+++ gdown/download.py
@@ -104,7 +104,9 @@ def download(
 
     file_id, is_download_link = parse_url(url)
 
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"  # NOQA
+    }
 
     while True:
 
