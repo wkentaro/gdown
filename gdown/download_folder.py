9874a4bbb8110e805c3f684e31c2850c43b41e65
Giuliano Oliveira <giuliano.programador@gmail.com>
2021-10-17 14:20:59 -0400

    refactor: using custom classes instances instead of dicts

--- gdown/download_folder.py
+++ gdown/download_folder.py
@@ -23,7 +23,47 @@ string_regex = re.compile(r"'((?:[^'\\]|\\.)*)'")
 MAX_NUMBER_FILES = 50
 
 
-def get_folder_list(folder, quiet=False, use_cookies=True, remaining_ok=False):
+class GoogleDriveFile(object):
+    """Represent Google Drive file objects structure.
+
+    Attributes
+    ----------
+    id: str
+        Unique id, used to build the download URL.
+    name: str
+        Actual name, used as file name.
+    type: str
+        MIME type, or application/vnd.google-apps.folder if it is a folder
+    children: List[GoogleDriveFile]
+        If it is a directory, it contains the folder files/directories
+
+    """
+
+    def __init__(self, id, name, type, children=None):
+        self.id = id
+        self.name = name
+        self.type = type
+        self.children = children if children is not None else []
+
+    def is_folder(self):
+        return self.type == folder_type
+
+    def __repr__(self):
+        template = "(id={id}, name={name}, type={type}, children={children})"
+        return "GoogleDriveFile" + template.format(
+            id=self.id,
+            name=self.name,
+            type=self.type,
+            children=self.children,
+        )
+
+
+def download_and_parse_google_drive_link(
+    folder,
+    quiet=False,
+    use_cookies=True,
+    remaining_ok=False,
+):
     """Get folder structure of Google Drive folder URL.
 
     Parameters
@@ -45,12 +85,10 @@ def get_folder_list(folder, quiet=False, use_cookies=True, remaining_ok=False):
     return_code: bool
         Returns False if the download completed unsuccessfully.
         May be due to invalid URLs, permission errors, rate limits, etc.
-    folder_list: dict
+    gdrive_file: GoogleDriveFile
         Returns the folder structure of the Google Drive folder.
     """
     return_code = True
-
-    folder_list = {}
 
     folder_page = client.get(folder)
 
@@ -87,51 +125,48 @@ def get_folder_list(folder, quiet=False, use_cookies=True, remaining_ok=False):
 
     folder_contents = [] if folder_arr[0] is None else folder_arr[0]
 
-    folder_list["file_name"] = folder_soup.title.contents[0][:-15]
-    folder_list["file_id"] = folder[39:]
-    folder_list["file_type"] = folder_type
-    folder_list["file_contents"] = []
+    gdrive_file = GoogleDriveFile(
+        id=folder[39:],
+        name=folder_soup.title.contents[0][:-15],
+        type=folder_type,
+    )
 
-    folder_file_list = [i[0] for i in folder_contents]
-    folder_name_list = [i[2] for i in folder_contents]
-    folder_type_list = [i[3] for i in folder_contents]
+    id_name_type_iter = ((e[0], e[2], e[3]) for e in folder_contents)
 
-    for file in range(len(folder_file_list)):
-        if folder_type_list[file] != folder_type:
+    for child_id, child_name, child_type in id_name_type_iter:
+        if child_type != folder_type:
             if not quiet:
                 print(
                     "Processing file",
-                    folder_file_list[file],
-                    folder_name_list[file],
+                    child_id,
+                    child_name,
                 )
-            folder_list["file_contents"].append(
-                {
-                    "file_name": folder_name_list[file],
-                    "file_id": folder_file_list[file],
-                    "file_type": folder_type_list[file],
-                    "file_contents": None,
-                }
+            gdrive_file.children.append(
+                GoogleDriveFile(
+                    id=child_id,
+                    name=child_name,
+                    type=child_type,
+                )
             )
             if not return_code:
                 return return_code, None
-        else:
-            if not quiet:
-                print(
-                    "Retrieving folder",
-                    folder_file_list[file],
-                    folder_name_list[file],
-                )
-            return_code, folder_structure = get_folder_list(
-                folders_url + folder_file_list[file],
-                use_cookies=use_cookies,
-                quiet=quiet,
+            continue
+
+        if not quiet:
+            print(
+                "Retrieving folder",
+                child_id,
+                child_name,
             )
-            if not return_code:
-                return return_code, None
-            folder_list["file_contents"].append(folder_structure)
-    has_at_least_max_files = (
-        len(folder_list["file_contents"]) == MAX_NUMBER_FILES
-    )
+        return_code, child = download_and_parse_google_drive_link(
+            folders_url + child_id,
+            use_cookies=use_cookies,
+            quiet=quiet,
+        )
+        if not return_code:
+            return return_code, None
+        gdrive_file.children.append(child)
+    has_at_least_max_files = len(gdrive_file.children) == MAX_NUMBER_FILES
     if not remaining_ok and has_at_least_max_files:
         err_msg = " ".join(
             [
@@ -143,16 +178,16 @@ def get_folder_list(folder, quiet=False, use_cookies=True, remaining_ok=False):
             ]
         )
         raise RuntimeError(err_msg)
-    return return_code, folder_list
+    return return_code, gdrive_file
 
 
-def get_directory_structure(directory, previous_path):
+def get_directory_structure(gdrive_file, previous_path):
     """Converts a Google Drive folder structure into a local directory list.
 
     Parameters
     ----------
-    directory: dict
-        Dictionary containing the Google Drive folder structure.
+    gdrive_file: GoogleDriveFile
+        Google Drive folder structure.
     previous_path: pathlib.Path
         Path containing the parent's file path.
 
@@ -162,19 +197,13 @@ def get_directory_structure(directory, previous_path):
         List containing a tuple of the files' ID and file path.
     """
     directory_structure = []
-    for file in directory["file_contents"]:
-        if file["file_type"] == folder_type:
-            directory_structure.append(
-                (None, previous_path / file["file_name"])
-            )
-            for i in get_directory_structure(
-                file, previous_path / file["file_name"]
-            ):
+    for file in gdrive_file.children:
+        if file.is_folder():
+            directory_structure.append((None, previous_path / file.name))
+            for i in get_directory_structure(file, previous_path / file.name):
                 directory_structure.append(i)
-        elif file["file_contents"] is None:
-            directory_structure.append(
-                (file["file_id"], previous_path / file["file_name"])
-            )
+        elif not file.children:
+            directory_structure.append((file.id, previous_path / file.name))
     return directory_structure
 
 
@@ -222,7 +251,7 @@ def download_folder(
     """
     if not quiet:
         print("Retrieving folder list")
-    return_code, folder_list = get_folder_list(
+    return_code, gdrive_file = download_and_parse_google_drive_link(
         folder,
         quiet=quiet,
         use_cookies=False,
@@ -238,11 +267,8 @@ def download_folder(
         output = Path.cwd()
     else:
         output = Path(output)
-    root_folder = output / folder_list["file_name"]
-    directory_structure = get_directory_structure(
-        folder_list,
-        root_folder,
-    )
+    root_folder = output / gdrive_file.name
+    directory_structure = get_directory_structure(gdrive_file, root_folder)
     root_folder.mkdir(parents=True, exist_ok=True)
 
     if not quiet:
