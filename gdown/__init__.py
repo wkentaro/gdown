e2ee80ddbccc6c596867bd32ef3a5bafe228a962
Giuliano Oliveira <giuliano.programador@gmail.com>
2021-10-17 16:07:14 -0400

    feat: export folder feature

--- gdown/__init__.py
+++ gdown/__init__.py
@@ -6,6 +6,9 @@ from .download_folder import download_folder
 from .cached_download import cached_download
 from .cached_download import md5sum
 from .download import download
+from .download_folder import download_folder
+from .download_folder import parse_google_drive_file
+from .download_folder import GoogleDriveFile
 from .extractall import extractall
 
 __author__ = "Kentaro Wada <www.kentaro.wada@gmail.com>"
