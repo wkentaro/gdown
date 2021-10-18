c39cd654d4c205744bc7f539d4d88229c706e3b0
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-10-18 10:18:21 +0100

    Remove dependency on pathlib

--- setup.py
+++ setup.py
@@ -55,8 +55,6 @@ setup(
         "six",
         "tqdm",
         "beautifulsoup4",
-        "pathlib2",
-        "future",
     ],
     description="Google Drive direct download of big files.",
     long_description=get_long_description(),
