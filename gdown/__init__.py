8fbf30c62ab20b8161ce6635c82360db9a60f853
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-10-18 09:52:35 +0100

    Stop exposing unnecessary functions

--- gdown/__init__.py
+++ gdown/__init__.py
@@ -2,13 +2,10 @@
 
 import pkg_resources
 
-from .download_folder import download_folder
 from .cached_download import cached_download
 from .cached_download import md5sum
 from .download import download
 from .download_folder import download_folder
-from .download_folder import parse_google_drive_file
-from .download_folder import GoogleDriveFile
 from .extractall import extractall
 
 __author__ = "Kentaro Wada <www.kentaro.wada@gmail.com>"
