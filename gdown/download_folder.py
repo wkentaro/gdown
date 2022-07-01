1de787de60a03fdd1a8b48f5126dd01bc0d4a783
Sabih Ismail <sabih.ismail@gmail.com>
2022-07-01 13:51:16 -0400

    Unify dash types

--- gdown/download_folder.py
+++ gdown/download_folder.py
@@ -117,12 +117,10 @@ def parse_google_drive_file(folder, content, use_cookies=True):
 
     folder_contents = [] if folder_arr[0] is None else folder_arr[0]
 
-    seps = [" - ", " – "]  # unicode dash and endash
-    for sep in seps:
-        splitted = folder_soup.title.contents[0].split(sep)
-        if len(splitted) >= 2:
-            name = sep.join(splitted[:-1])
-            break
+    separator = " - "
+    splitted = folder_soup.title.contents[0].replace(" – ", separator).split(separator) # unify dash types
+    if len(splitted) >= 2:
+        name = separator.join(splitted[:-1])
 
     gdrive_file = GoogleDriveFile(
         id=folder.split("/")[-1],
