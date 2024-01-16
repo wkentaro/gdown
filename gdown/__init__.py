897b49751ad4f2e617ae8d7fb045cf89d3d59d47
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-01-16 22:21:38 +0900

    4.7.3

--- gdown/__init__.py
+++ gdown/__init__.py
@@ -8,4 +8,4 @@ from .download_folder import download_folder
 from .extractall import extractall
 
 __author__ = "Kentaro Wada <www.kentaro.wada@gmail.com>"
-__version__ = "4.7.2"
+__version__ = "4.7.3"
