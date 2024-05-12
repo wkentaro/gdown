43140b5e968f905e99cb161d0ad11dae1a084f8e
Kentaro Wada <www.kentaro.wada@gmail.com>
2024-05-12 13:27:42 +0900

    Use TemporaryDirectory for Windows

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,12 +1,12 @@
 import os
+import tempfile
 
 from gdown.download import download
 
 
 def test_download():
-    url = "https://raw.githubusercontent.com/wkentaro/gdown/3.1.0/gdown/__init__.py"  # NOQA
-    output = "/tmp/gdown_r"
-
-    # Usage before https://github.com/wkentaro/gdown/pull/32
-    assert download(url, output, quiet=False) == output
-    os.remove(output)
+    with tempfile.TemporaryDirectory() as d:
+        file_path = os.path.join(d, "file")
+        url = "https://raw.githubusercontent.com/wkentaro/gdown/3.1.0/gdown/__init__.py"  # NOQA
+        # Usage before https://github.com/wkentaro/gdown/pull/32
+        assert download(url=url, output=file_path, quiet=False) == file_path
